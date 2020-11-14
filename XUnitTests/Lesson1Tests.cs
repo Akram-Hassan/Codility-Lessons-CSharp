dd2f95a711c93be9f148855120d13f7f62867705
Akram Hassan <akram.imam82@gmail.com>
2020-11-14 16:23:01 +0200

    Add renamed class

---
+++ XUnitTests/Lesson1Tests.cs
@@ -0,0 +1,15 @@
+using Codility_Lession_CSharp;
+using System;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class Lesson1Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(0 ,Lesson1.Solution(4));
+        }
+    }
+}
