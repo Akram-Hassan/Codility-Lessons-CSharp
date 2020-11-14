4c7eb458564382df76c89723bee427a93569ff17
Akram Hassan <akram.imam82@gmail.com>
2020-11-14 16:17:11 +0200

    Setup for lesson 1

---
+++ Codility-Lession-CSharp/Lesson1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility_Lession_CSharp
+{
+    public class Lesson1
+    {
+        public static int Solution(int N)
+        {
+            return 0;
+        }
+    }
+}
