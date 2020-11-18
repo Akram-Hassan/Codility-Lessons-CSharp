1c8a7dd1caec4d9a8f035dd0b0792ddf63366adf
Akram Hassan <akram.imam82@gmail.com>
2020-11-18 11:53:02 +0200

    Setup for Lesson2.1: Cyclic Rotation

---
+++ Codility-Lession-CSharp/Lesson2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility_Lession_CSharp
+{
+    namespace Lesson2
+    {
+        public class CyclicRotation
+        {
+
+        }
+    }
+}
---
+++ XUnitTests/Lesson2Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTests
+{
+    namespace Lesson2Tests
+    {
+        public class CyclicRotationTests
+        {
+            [Fact]
+            public void Test1()
+            {
+
+            }
+        }
+    }
+}
