dc90f3ce040d58918f37252d17ab8b736b3ee792
Akram Hassan <akram.imam82@gmail.com>
2020-11-18 19:17:23 +0200

    Lesson2: Cyclic Rotation submitted, 100% passed

--- Codility-Lession-CSharp/Lesson2.cs
+++ Codility-Lession-CSharp/Lesson2.cs
@@ -8,7 +8,26 @@ namespace Codility_Lession_CSharp
     {
         public class CyclicRotation
         {
+            public static int[] solution(int[] A, int K)
+            {
+                int[] result = new int[A.Length];
 
+                for (int i = 0; i < A.Length; i++)
+                {
+                    int index = CalculateNewIndex(i, K, A.Length);
+                    result[index] = A[i];
+                }
+
+                return result;
+            }
+
+            public static int CalculateNewIndex(int index, int K, int length)
+            {
+                if (index > (length - 1))
+                    throw new ArgumentException("Invalid index value " + index + ", Array length is " + length);
+                
+                return (index + K) % length;
+            }
         }
     }
 }
--- XUnitTests/Lesson2Tests.cs
+++ XUnitTests/Lesson2Tests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Codility_Lession_CSharp.Lesson2;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -9,10 +10,37 @@ namespace XUnitTests
     {
         public class CyclicRotationTests
         {
-            [Fact]
-            public void Test1()
-            {
+            [Theory]
+            [InlineData(3, 0, 3, 5)]
+            [InlineData(4, 1, 3, 5)]
+            [InlineData(0, 2, 3, 5)]
+            [InlineData(1, 3, 3, 5)]
+            [InlineData(2, 4, 3, 5)]
 
+            [InlineData(0, 0, 0, 4)]
+            [InlineData(0, 0, 0, 3)]
+            [InlineData(0, 0, 0, 30)]
+
+            public void TestCalculateLength(int expected, int index, int K, int Length)
+            {
+                Assert.Equal(expected, CyclicRotation.CalculateNewIndex(index, K, Length));
+            }
+
+            [Fact]
+            public void TestCalculateNewIndexException()
+            {
+                Assert.Throws<ArgumentException>(
+                    () => CyclicRotation.CalculateNewIndex(5, 1, 3));
+            }
+
+            [Theory]
+            [InlineData(
+                new int[] { 9, 7, 6, 3, 8 },
+                new int[] { 3, 8, 9, 7, 6 },
+                3)]
+            public void MainTest(int[] expected, int[] A, int k)
+            {
+                Assert.Equal(expected, CyclicRotation.solution(A, k));
             }
         }
     }
