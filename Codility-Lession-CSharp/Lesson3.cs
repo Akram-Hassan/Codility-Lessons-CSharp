1db59af5214c571d84502ba1023159f6e69939d5
Akram Hassan <akram.imam82@gmail.com>
2020-11-19 03:11:02 +0200

    Lesson 3 - FrogJmp : 100% passed

--- Codility-Lession-CSharp/Lesson3.cs
+++ Codility-Lession-CSharp/Lesson3.cs
@@ -6,6 +6,12 @@ namespace Codility_Lession_CSharp
 {
     namespace Lesson3
     {
-
+        public class FrogJmp
+        {
+            public static int solution(int X, int Y, int D)
+            {
+                return (int) Math.Ceiling((Y - X) / (double) D);
+            }
+        }
     }
 }
--- XUnitTests/Lesson3Tests.cs
+++ XUnitTests/Lesson3Tests.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Codility_Lession_CSharp.Lesson3;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace XUnitTests
 {
     namespace Lesson3Tests
     {
-
+        public class FrogJmpTests
+        {
+            [Theory]
+            [InlineData(3,10,85,30)]
+            public void MainTest(int expected, int X, int Y, int D)
+            {
+                Assert.Equal(expected, FrogJmp.solution(X, Y, D));
+            }
+        }
     }
 }
