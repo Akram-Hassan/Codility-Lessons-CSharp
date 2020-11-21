c31215e55fc999a9d5a4fe81723b6c957d768c08
Akram Hassan <akram.imam82@gmail.com>
2020-11-21 07:17:13 +0200

    Lesson 4 - FrogRiverOne.solution1: 100% passed

--- Codility-Lession-CSharp/Lesson4.cs
+++ Codility-Lession-CSharp/Lesson4.cs
@@ -8,9 +8,28 @@ namespace Codility_Lession_CSharp
     {
         public class FrogRiverOne
         {
-            public static int solution(int X, int[] A)
+            public static int solution1(int X, int[] A)
             {
-                return 0;
+                int total = X * (X + 1) / 2;
+                int sum = 0;
+                var set = new HashSet<int>();
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    int item = A[i];
+                    if (!set.Contains(item))
+                    {
+                        set.Add(item);
+                        if (item <= X)
+                        {
+                            sum += item;
+                            if (sum == total)
+                                return i;
+                        }
+                    }
+                }
+
+                return -1;
             }
         }
     }
--- XUnitTests/Lesson4Tests.cs
+++ XUnitTests/Lesson4Tests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Codility_Lession_CSharp.Lesson4;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -9,10 +10,21 @@ namespace XUnitTests
     {
         public class FrogRiverOneTests
         {
-            [Fact]
-            public void MainTest()
-            {
+            [Theory]
+            [InlineData(6, 5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 })]
+            [InlineData(4, 4, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 })]
+            [InlineData(4, 3, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 })]
+            [InlineData(2, 3, new int[] { 1, 3, 2 })]
+            [InlineData(3, 4, new int[] { 4, 1, 3, 2 })]
+            [InlineData(-1, 5, new int[] { 1, 3, 2 })]
+            [InlineData(2, 2, new int[] { 1, 3, 2 })]
+            
+            [InlineData(8, 2, new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 2 })]
+            [InlineData(0, 1, new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 2 })]
 
+            public void TestSolution1(int expected, int X, int[] A)
+            {
+                Assert.Equal(expected, FrogRiverOne.solution1(X, A));
             }
         }
     }
